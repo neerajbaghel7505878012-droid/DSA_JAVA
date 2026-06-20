1113e86f2b864ba20b8577179e86f8f9b3096612
Neeraj baghel <neerajbaghel7505878012@gmail.com>
2026-06-21 00:54:07 +0530

    Add files via upload

---
+++ Array/ContainsDuplicate.java
@@ -0,0 +1,30 @@
+
+
+
+public class ContainsDuplicate {
+public static boolean checkDuplicate(int num[]){
+    
+    for(int i=0;i<num.length;i++){
+        for(int j=i+1;j<num.length;j++){
+           if(num[i]==num[j]){
+            return true;
+
+           }
+        }
+    }
+   return false;
+    
+}
+   
+    public static void main(String[] args) {
+int num[]={1,2,3,1};
+System.out.println(checkDuplicate(num));
+
+
+        
+    }
+}
+
+
+    
+
