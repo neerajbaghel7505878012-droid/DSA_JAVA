661ff5a2d763377b08e2dbed7798342f932cbfc1
Neeraj baghel <neerajbaghel7505878012@gmail.com>
2026-06-23 00:44:25 +0530

    Add files via upload

--- Array/MergeSortedArray.java
+++ Array/MergeSortedArray.java
@@ -28,10 +28,31 @@ public class MergeSortedArray {
 
 
     }
+    public static void mergeOptimal(int nums1[],int m,int nums2[],int n){
+        int i=m-1;
+        int j=n-1;
+        int k=(n+m)-1;
+        while(i>=0 && j>=0){
+            if(nums1[i]<nums2[j]){
+                nums1[k]=nums2[j];
+                k--;
+                j--;
+            }else{
+                nums1[k]=nums1[i];
+                k--;
+                i--;
+            }
+        }
+        while(j>=0){
+             nums1[k]=nums2[j];
+                k--;
+                j--;
+        }
+    }
     public static void main(String[] args) {
         int num1[]={1,2,3,0,0,0};
         int num2[]={1,2,3};
-        mergeUsingTemp(num1, 3, num2, 3);
+        mergeOptimal(num1, 3, num2, 3);
         for(int i :num1){
             System.out.print(i+"  ");
         }
