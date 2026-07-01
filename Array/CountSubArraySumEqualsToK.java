718268c5536317bd5c3f2812dd64ecbf84861dec
Neeraj baghel <neerajbaghel7505878012@gmail.com>
2026-07-01 19:04:11 +0530

    Add files via upload

--- Array/CountSubArraySumEqualsToK.java
+++ Array/CountSubArraySumEqualsToK.java
@@ -1,6 +1,9 @@
+import java.util.HashMap;
+
 public class CountSubArraySumEqualsToK {
     // BruteForce Approach 
     //Time Complexity - O(n^3)
+    //Space Complexity - O(1)
     public static int bruteforce(int arr[],int k){
         int count=0;
         for(int i=0;i<arr.length;i++){
@@ -18,6 +21,7 @@ public class CountSubArraySumEqualsToK {
     }
     // Better Approach 
     //Time Complexity - O(n^2)
+    //Space Complexity - O(1)
     public static int betterSolution(int arr[],int k){
      int count=0;
         for(int i=0;i<arr.length;i++){
@@ -34,36 +38,28 @@ public class CountSubArraySumEqualsToK {
     // Optimal Solution using HashMap
     //Time Complexity - O(n)
     //Space Complexity - O(n)
-    public static boolean linearSearch(int prefix[],int sum){
-        for(int i=0;i<prefix.length;i++){
-            if(prefix[i]==sum){
-                return true;
-            }
-        }
-        return false
-        ;
-    }
+   
     public static int optimalSolution(int arr[],int k){
-        int count = 0;
-        int prefix[]=new int[arr.length];
-        prefix[0]=arr[0];
-        for(int i=1;i<arr.length;i++){
-            prefix[i]=prefix[i-1]+arr[i];
+        if(arr.length==0) return 0;
+       HashMap <Integer,Integer> map=new HashMap<>();
+       map.put(0, 1);
+       int count=0;
+       int prefixsum=0;
+       for(int i=0;i<arr.length;i++){
+        prefixsum+=arr[i];
+        if(map.containsKey(prefixsum-k)){
+            count+=map.get(prefixsum-k);
         }
-    for(int i=0;i<prefix.length;i++){
-        int sum=prefix[i]-k;
-        if(sum==0){
-            count++;
-        }else if(linearSearch(prefix, sum)){
-            count++;
-        }
-    }
-return count;
+      
+        map.put(prefixsum, map.getOrDefault(prefixsum, 0)+1);
+        
+       }
+       return count;
     }
 public static void main(String[] args) {
-        int arr[]={3,1,2,4};
+        int arr[]={};
         // System.out.println(bruteforce(arr, 6));
       //  System.out.println(betterSolution(arr, 4));
-      System.out.println(optimalSolution(arr, 2));
+      System.out.println(optimalSolution(arr, 3));
     }
 }
